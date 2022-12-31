63428be68c1844a41e275034bbdf5b0731e95a49
scode234 <scodes3451@gmail.com>
2022-12-30 21:00:30 -0600

    Add user prompts to get currency code and amount

---
+++ CurrencyConverter/src/currencyConverter/UserPrompts.java
@@ -0,0 +1,23 @@
+package currencyConverter;
+
+import java.util.Scanner;
+
+public  class UserPrompts {
+	public static String promptUserCurrencyCode() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(
+				"Please select a currency:\n1 - EUR (Euro)\n2 - USD (US Dollar)\n3 - GBP (British Pound Sterling)"
+				+ "\n4 - AUD (Australian Dollar)\n5 - CAD ( Canadian Dollar)\n6 - BRL (Brazilian Real)\n7 - CNY (Chinese Yuan)\n8 - HKD (Hong Kong Dollar)"
+				+ "\n9 - MXN (Mexican Peso)\n10 - INR (Indian Rupee)\n11 - KRW (South Korean Won)\n12 - JPY (Japanese Yen)");
+		String code = scanner.next();
+		return code;
+	}
+
+	public static double promptUserValue() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter a value to convert:");
+		double value = scanner.nextDouble();
+		return value;
+
+	}
+}
