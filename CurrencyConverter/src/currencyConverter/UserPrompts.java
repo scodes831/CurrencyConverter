728b9017abea9a9f185c0bfcb8ca1ea28df42e4e
scode234 <scodes3451@gmail.com>
2023-01-01 13:10:27 -0600

    Change to int return type

--- CurrencyConverter/src/currencyConverter/UserPrompts.java
+++ CurrencyConverter/src/currencyConverter/UserPrompts.java
@@ -3,13 +3,13 @@ package currencyConverter;
 import java.util.Scanner;
 
 public  class UserPrompts {
-	public static String promptUserCurrencyCode(String message) {
+	public static int promptUserCurrencyCode(String message) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println(
 				"Please select " + message + ":\n1 - EUR (Euro)\n2 - USD (US Dollar)\n3 - GBP (British Pound Sterling)"
 				+ "\n4 - AUD (Australian Dollar)\n5 - CAD ( Canadian Dollar)\n6 - BRL (Brazilian Real)\n7 - CNY (Chinese Yuan)\n8 - HKD (Hong Kong Dollar)"
 				+ "\n9 - MXN (Mexican Peso)\n10 - INR (Indian Rupee)\n11 - KRW (South Korean Won)\n12 - JPY (Japanese Yen)");
-		String code = scanner.next();
+		int code = scanner.nextInt();
 		return code;
 	}
 
