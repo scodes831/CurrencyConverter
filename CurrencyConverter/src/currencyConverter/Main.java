8eb2269301987b5abad8e0fb16e44aff5e722429
scode234 <scodes3451@gmail.com>
2023-01-02 17:18:13 -0600

    Remove header and end characters from response file

--- CurrencyConverter/src/currencyConverter/Main.java
+++ CurrencyConverter/src/currencyConverter/Main.java
@@ -1,10 +1,27 @@
 package currencyConverter;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import org.apache.commons.io;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
-	public static void main(String[] args) {
-		CurrencyConverter currencyConverter = new CurrencyConverter();
-		ArrayList<Currency> currencyInputs = currencyConverter.promptUserForInputs(); 
+	public static void main(String[] args) throws IOException, InterruptedException {
+//		CurrencyConverter currencyConverter = new CurrencyConverter();
+//		ArrayList<Currency> currencyInputs = currencyConverter.promptUserForInputs();
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest getRequest = HttpRequest.newBuilder()
+				.uri(URI.create("https://api.freecurrencyapi.com/v1/latest"))
+				.header("apikey", "nGsxt3T0lFZPs5hEhaDkg8eSDO66UsWa8PKQp1jR")
+				.GET()
+				.build();
+		HttpResponse<String> getResponse = client.send(getRequest, BodyHandlers.ofString());
+		System.out.println(getResponse.body());
+		//make method to generate map of currency:exchange rate
 	}
 }
