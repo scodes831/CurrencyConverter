e7871f3e9b925a0d51f5a2b7f645b72df7c471f2
scode234 <scodes3451@gmail.com>
2023-01-03 18:27:51 -0600

    Add API call to get currency exchange rates

--- CurrencyConverter/src/currencyConverter/Main.java
+++ CurrencyConverter/src/currencyConverter/Main.java
@@ -1,27 +1,28 @@
 package currencyConverter;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-
-import org.apache.commons.io;
-import org.apache.commons.io.IOUtils;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
-//		CurrencyConverter currencyConverter = new CurrencyConverter();
-//		ArrayList<Currency> currencyInputs = currencyConverter.promptUserForInputs();
+		CurrencyConverter currencyConverter = new CurrencyConverter();
+		ArrayList<Currency> currencyInputs = currencyConverter.promptUserForInputs();
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest getRequest = HttpRequest.newBuilder()
 				.uri(URI.create("https://api.freecurrencyapi.com/v1/latest"))
-				.header("apikey", "nGsxt3T0lFZPs5hEhaDkg8eSDO66UsWa8PKQp1jR")
+				.header("apikey", "apikey")
 				.GET()
 				.build();
 		HttpResponse<String> getResponse = client.send(getRequest, BodyHandlers.ofString());
 		System.out.println(getResponse.body());
-		//make method to generate map of currency:exchange rate
 	}
 }
+
+
