dfbeffd68c3939e086b3eed02e8fe1f5c84253f7
scode234 <scodes3451@gmail.com>
2023-01-03 18:28:41 -0600

    Add method and helper methods to format API Http response to HashMap

--- CurrencyConverter/src/currencyConverter/Data.java
+++ CurrencyConverter/src/currencyConverter/Data.java
@@ -1,6 +1,27 @@
 package currencyConverter;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Data {
+	
+	public static Map<String,BigDecimal> convertResponseToMap(String response) {
+		HashMap<String,BigDecimal> responseMap = new HashMap<String,BigDecimal>();
+		String[] currenciesArr = response.split(",");
+		for (String s : currenciesArr) {
+			String[] currencyInfo = s.split(":");
+			String currencyCode = removePunctuation(currencyInfo[0]);
+			BigDecimal exchangeRate = new BigDecimal(currencyInfo[1]);
+			responseMap.put(currencyCode, exchangeRate);
+		}
+		return responseMap;
+	}
+	
+	private static String removePunctuation(String input) {
+		String withoutPunctuation =  input.replaceAll("\\p{P}", "");
+		return withoutPunctuation;
+	}
 	
 	public static String formatStringResponse(String response) {
 		int headerIndex = calculateHeaderIndex(response);
