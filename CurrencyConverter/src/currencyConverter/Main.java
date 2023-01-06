b934862353d09f0251c01c19e98da91695db7806
scode234 <scodes3451@gmail.com>
2023-01-05 19:34:55 -0600

    Wrap in a do while loop and ask user if they want to convert another currency

--- CurrencyConverter/src/currencyConverter/Main.java
+++ CurrencyConverter/src/currencyConverter/Main.java
@@ -10,44 +10,53 @@ import java.util.HashMap;
 
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		CurrencyConverter currencyConverter = new CurrencyConverter();
-		currencyConverter.promptUserForInputs();
-		HttpURLConnection connection;
-		
-		try {
+		boolean goAgain = true;
+		do {
+			CurrencyConverter currencyConverter = new CurrencyConverter();
+			currencyConverter.promptUserForInputs();
+			HttpURLConnection connection;
 			
-			URL url = new URL("https://api.freecurrencyapi.com/v1/latest");
-			connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-			connection.setRequestProperty("apikey", "nGsxt3T0lFZPs5hEhaDkg8eSDO66UsWa8PKQp1jR");
-			StringBuilder response;
-			
-			try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-				String line;
-				response = new StringBuilder();
+			try {
 				
-				while ((line = in.readLine()) != null) {
-					response.append(line);
+				URL url = new URL("https://api.freecurrencyapi.com/v1/latest");
+				connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("GET");
+				connection.setRequestProperty("apikey", "nGsxt3T0lFZPs5hEhaDkg8eSDO66UsWa8PKQp1jR");
+				StringBuilder response;
+				
+				try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+					String line;
+					response = new StringBuilder();
+					
+					while ((line = in.readLine()) != null) {
+						response.append(line);
+					}
 				}
+				
+				if (connection.getResponseCode() == 200 ) {
+					String formattedInput = Data.formatStringResponse(response.toString());
+					HashMap<String,BigDecimal> inputMap = Data.convertResponseToMap(formattedInput);
+					currencyConverter.findExchangeRates(inputMap);
+					currencyConverter.calculateExchangeValue();
+					System.out.println(currencyConverter.getOrigCurrency().getValue() + " "
+							+ currencyConverter.getOrigCurrency().getCode() + " = " + currencyConverter.getNewCurrency().getValue() + " " + currencyConverter.getNewCurrency().getCode());
+					boolean goAgainInput = UserPrompts.promptUserGoAgain();
+					if (!goAgainInput) {
+						goAgain = false;
+					}
+					connection.disconnect();
+				} else if (connection.getResponseCode() < 500) {
+					throw new Exception("There has been an error processing your request, please try again.");
+				} else {
+					throw new Exception("Internal Server Error");
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 			
-			if (connection.getResponseCode() == 200 ) {
-				String formattedInput = Data.formatStringResponse(response.toString());
-				HashMap<String,BigDecimal> inputMap = Data.convertResponseToMap(formattedInput);
-				currencyConverter.findExchangeRates(inputMap);
-				currencyConverter.calculateExchangeValue();
-				System.out.println(currencyConverter.getOrigCurrency().getValue() + " "
-						+ currencyConverter.getOrigCurrency().getCode() + " = " + currencyConverter.getNewCurrency().getValue() + " " + currencyConverter.getNewCurrency().getCode());
-				connection.disconnect();
-			} else if (connection.getResponseCode() < 500) {
-				throw new Exception("There has been an error processing your request, please try again.");
-			} else {
-				throw new Exception("Internal Server Error");
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		} while (goAgain);
+		
+		
 	}
 }
