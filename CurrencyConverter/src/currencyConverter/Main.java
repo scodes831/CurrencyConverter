49c82b81dc6d4053ad24d173fdeb30f99d213b64
scode234 <scodes3451@gmail.com>
2023-01-05 18:21:54 -0600

    Refactor Main method using HttpURLConnection to get http response in string format

--- CurrencyConverter/src/currencyConverter/Main.java
+++ CurrencyConverter/src/currencyConverter/Main.java
@@ -1,28 +1,46 @@
 package currencyConverter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.ArrayList;
-import java.util.Map;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
+		
 		CurrencyConverter currencyConverter = new CurrencyConverter();
-		ArrayList<Currency> currencyInputs = currencyConverter.promptUserForInputs();
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest getRequest = HttpRequest.newBuilder()
-				.uri(URI.create("https://api.freecurrencyapi.com/v1/latest"))
-				.header("apikey", "apikey")
-				.GET()
-				.build();
-		HttpResponse<String> getResponse = client.send(getRequest, BodyHandlers.ofString());
-		System.out.println(getResponse.body());
+		currencyConverter.promptUserForInputs();
+		HttpURLConnection connection;
+		
+		try {
+			
+			URL url = new URL("https://api.freecurrencyapi.com/v1/latest");
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("apikey", "nGsxt3T0lFZPs5hEhaDkg8eSDO66UsWa8PKQp1jR");
+			StringBuilder response;
+			
+			try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+				String line;
+				response = new StringBuilder();
+				
+				while ((line = in.readLine()) != null) {
+					response.append(line);
+				}
+			}
+			String formattedInput = Data.formatStringResponse(response.toString());
+			HashMap<String,BigDecimal> inputMap = Data.convertResponseToMap(formattedInput);
+			currencyConverter.findExchangeRates(inputMap);
+			currencyConverter.calculateExchangeValue();
+			System.out.println(currencyConverter.getOrigCurrency().getValue() + " "
+					+ currencyConverter.getOrigCurrency().getCode() + " = " + currencyConverter.getNewCurrency().getValue() + " " + currencyConverter.getNewCurrency().getCode());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
 	}
 }
-
-
